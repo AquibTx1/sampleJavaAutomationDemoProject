6bdcb783251496cc6522e782c416122599cb0939
AquibTx1 <99241917+AquibTx1@users.noreply.github.com>
2023-01-11 21:03:21 +0530

    Update createbug.ps1

--- createbug.ps1
+++ createbug.ps1
@@ -83,7 +83,7 @@ $projects.value  | ForEach-Object {
     $workitemType = "Issue"
     $Area = "newdemo"
     $AssignedTo = "MOHAMMAD SHABAN"
-    $Reason = "To Do"
+    $Reason = "New"
     $tags = "Automation"
     $tfsBaseUrl = GetUrl -orgUrl $orgUrl -header $header -AreaId $testAreaId
     $tfsWorkTrackingItemUrl = GetUrl -orgUrl $orgUrl -header $header -AreaId $workTrackingAreaId
